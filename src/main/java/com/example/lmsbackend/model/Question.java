ecd3e8fa0db6e3cbe675d4b8145b56dcdb5444b0
Touse21 <touseeverything@gmail.com>
2025-04-20 11:05:08 +0530

    Add src/main/java/com/example/lmsbackend/model/Question.java 1745127306618

---
+++ src/main/java/com/example/lmsbackend/model/Question.java
@@ -0,0 +1,29 @@
+package com.example.lmsbackend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "questions")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 1000)
+    private String text;
+
+    @ManyToOne
+    @JoinColumn(name = "quiz_id")
+    private Quiz quiz;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Answer> answers = new HashSet<>();
+}
